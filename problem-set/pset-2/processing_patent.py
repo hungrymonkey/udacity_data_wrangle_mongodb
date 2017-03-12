bda3b3a1fe302beba540225c5697e87e6ce56552
moo <boringmonkey@gmail.com>
2017-03-11 22:40:43 -0800

    fixed a mistake with last commit, now correct

--- problem-set/pset-2/processing_patent.py
+++ problem-set/pset-2/processing_patent.py
@@ -1,3 +1,12 @@
+def split_file(filename):
+    """
+    Split the input file into separate files, each containing a single patent.
+    As a hint - each patent declaration starts with the same line that was
+    causing the error found in the previous exercises.
+    
+    The new files should be saved with filename in the following format:
+    "{}-{}".format(filename, n) where n is a counter, starting from 0.
+    """
     with open(filename, 'r') as rf:
         n = 0
         wf = open("{}-{}".format(filename, n), 'w')
@@ -9,5 +18,7 @@
                 wf.close()
                 n = n+1
                 wf = open("{}-{}".format(filename, n), 'w')
+		wf.write(line)
         wf.close()
         rf.close()
+
